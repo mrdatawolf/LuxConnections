6b7bb57d29ea056b305d0562a4ac5c70aad856d5
Patrick Moon <patrickmoon@outlook.com>
2021-05-05 21:44:16 -0700

    All member data is livewire controlled.  Also added a dropdown to link  staff to a member. Also the staff cards now update when the staff link is changed on a member card.

--- app/Http/Livewire/StaffCard.php
+++ app/Http/Livewire/StaffCard.php
@@ -12,11 +12,35 @@ class StaffCard extends Component
     public $membersSupported;
     public $totalMembersSupported;
 
+    public $listeners = ['memberStaffLinkUpdated'];
+
     public function mount($staffId) {
         $this->staffId = $staffId;
 
         $this->getStaffName();
         $this->getMembersSupported();
+    }
+
+    public function memberStaffLinkUpdated($staffId, $memberId) {
+        $members = [
+            1 => '1llusionist#1981',
+            2 => '1one#1586',
+            3 => 'à¹–Ì¶Ì¶Ì¶Î¶ÍœÍ¡ GrimmÎ¶ÍœÍ¡à¹–#1817'
+        ];
+        if($staffId === $this->staffId) {
+            if(! in_array($memberId, array_keys($this->membersSupported))) {
+                $this->membersSupported[$memberId] = $members[$memberId];
+            }
+        } else {
+            if(in_array($memberId, array_keys($this->membersSupported))) {
+                if(isset($this->membersSupported[$memberId])) {
+                    unset($this->membersSupported[$memberId]);
+                }
+            }
+        }
+
+        $this->totalMembersSupported = count($this->membersSupported);
+
     }
 
     private function getStaffName() {
