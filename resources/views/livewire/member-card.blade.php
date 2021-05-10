8136539a61a241e85865fb95ae24234f36e928f3
Patrick Moon <patrickmoon@outlook.com>
2021-05-10 15:20:27 -0700

    Changed member and staff names to be text and they now link to the expanded page for the person. adjusted the spacing for the members listed under a staff member in the staff card.

--- resources/views/livewire/member-card.blade.php
+++ resources/views/livewire/member-card.blade.php
@@ -3,7 +3,7 @@
     <div class="col-span-2">
         <div class="{{ ($userHasAlias) ? 'hidden' : '' }}" title="Associate this member to your login account."><span wire:click="$set('showConfirmBurdenModal', true)" class="{{ ($isStaff) ? 'bg-green-300' : 'bg-blue-300' }} material-icons rounded-full">fitness_center</span></div>
     </div>
-    <div class="font-bold col-start-2 col-span-10"><x-jet-input type="text" class="discord_name p-0 m-0 w-full border-none" title="Member's discord name" value="{{ $memberName }}" disabled/></div>
+    <div class="font-bold col-span-12"><a href="{{ route('member',['id' => $member->id]) }}">{{ $memberName }}</a></div>
     <div class="col-span-12">
         <x-jet-input type="text" wire:model="fullDiscordId" wire:change="changeFullDiscordId" class="p-0 m-0 w-full border-none" value="{{ $fullDiscordId }}" placeholder="Full Discord Id"/>
     </div>
