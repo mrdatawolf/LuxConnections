f692a70aa8b5abf33f8a2f924e4bdc172dc6f566
Patrick Moon <patrickmoon@outlook.com>
2021-05-19 19:44:36 -0700

    if there are less attached members then we currently display for a staff card we should show the total users. Also cleaned up some wording. Also moved some css out of the blades and into css files. Also worked on the member view's look.

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('LUX Member') }}
+            {{ __('LUX Member2') }}
         </h2>
     </x-slot>
     <div class="grid grid-cols-12">
-        <div class="col-span-12">
+        <div class="col-start-2 col-span-10">
             @livewire('expanded-member-card',['memberId'=>$id])
         </div>
     </div>
